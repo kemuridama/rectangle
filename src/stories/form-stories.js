59434bb1ffb128234a8238b01054e92bd0336834
Ryo Ochiai <ryo.ochiai@kemuridama.jp>
2017-05-31 04:26:04 +0900

    Implement input field style
    Signed-off-by: Ryo Ochiai <ryo.ochiai@kemuridama.jp>

--- src/stories/form-stories.js
+++ src/stories/form-stories.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
 import '../styles.scss';
+import InputField from './form/input-field';
 import Checkbox from './form/checkbox';
 import RadioButton from './form/radio-button';
 
 /* global module */
 storiesOf('Form', module)
+  .add('InputField', () => {
+    return <InputField onChange={action('change')} />;
+  })
   .add('Checkbox', () => {
     return <Checkbox onChange={action('change')} />;
   })
